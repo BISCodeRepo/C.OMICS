9dc79ef913a4d2ddf5241976912e29734cb8e69a
zshanji <shanji920125@gmail.com>
2024-07-01 16:25:21 +0900

    back_end 추가

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from scipy import stats
 from flask_cors import CORS  
 import numpy as np
 import pandas as pd
-import json
 
 app = Flask(__name__)
 CORS(app, resources={r"/kruskal_wallis": {"origins": "http://127.0.0.1:5500",
@@ -17,8 +16,12 @@ CORS(app, resources={r"/kruskal_wallis": {"origins": "http://127.0.0.1:5500",
                      r"/get_gene_name_list": {"origins": "http://127.0.0.1:5500",
                                      "methods": ["POST"],
                                      "allow_headers": ["Content-Type"]
-                                     }
-                     r"/get_heatmap_data": {"origins": "http://127.0.0.1:5500",
+                                     },
+                     r"/get_nmf_immune_heatmap_data": {"origins": "http://127.0.0.1:5500",
+                                     "methods": ["POST"],
+                                     "allow_headers": ["Content-Type"]
+                                     },
+                     r"/get_gene_heatmap_data": {"origins": "http://127.0.0.1:5500",
                                      "methods": ["POST"],
                                      "allow_headers": ["Content-Type"]
                                      }
@@ -67,6 +70,16 @@ def get_heatmap_data():
     
     return nmf_df
 
+
+def get_heatmap_gene_name_list(gene_type,gene_site,uniprot_site):
+    tmp = gene_site
+    if gene_type == 'RNA':
+        return tmp +"_g"
+    elif gene_type == 'PROTEIN':
+        return tmp + "_p("+uniprot_site+")" 
+    else:
+        return tmp
+
                    
 @app.route('/kruskal_wallis', methods=['POST'])
 def kruskal_wallis():
@@ -110,37 +123,53 @@ def rankSums():
 def get_gene_name_list():
     try:
         data = request.json
-        print(data)
+        #print(data)
         nmf_df = get_heatmap_data()
     
         #print(nmf_df['search_gene_name'])
         gene_name_vals = set(nmf_df['search_gene_name'].tolist())
-        print(len(list(gene_name_vals)))
+        #print(len(list(gene_name_vals)))
         return jsonify({'geneName':list(gene_name_vals)})
     except Exception as e:
         return jsonify({"error": str(e)})
     
     
-@app.route('/get_heatmap_data', methods=['POST'])
-def get_heatmap_data():
+@app.route('/get_nmf_immune_heatmap_data', methods=['POST'])
+def get_nmf_immune_heatmap_data():
+    try:
+        data = request.json
+        gene_name_list = data['geneNameList']
+        print(gene_name_list)
+        
+        nmf_df = get_heatmap_data()
+
+        sub_nmf_df = nmf_df[nmf_df['search_gene_name'].apply(lambda x: any(keyword == x for keyword in gene_name_list))] 
+        sub_nmf_df['heatmap_gene_name'] = sub_nmf_df.apply(lambda x:get_heatmap_gene_name_list(x['Type'],x['GeneName_Site'],x['UniProt_Site']), axis=1)
+        print(len(sub_nmf_df))
+        
+        sub_nmf_sort = sub_nmf_df.sort_values(by=['GeneName_Site'],ascending=[True])
+        
+        return jsonify(sub_nmf_sort.to_json(orient='split'))
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+    
+    
+@app.route('/get_gene_heatmap_data', methods=['POST'])
+def get_gene_heatmap_data():
     try:
         data = request.json
         print(data)
         nmf_df = get_heatmap_data()
         
-        sub_nmf_df = nmf_df[nmf_df['search_gene_name']=='']
+        rna_df = nmf_df[nmf_df['Type']=='RNA']
+        print("RNA Total : "+str(len(rna_df)))
         
-    
-        #print(nmf_df['search_gene_name'])
-        gene_name_vals = set(nmf_df['search_gene_name'].tolist())
-        print(len(list(gene_name_vals)))
-        return jsonify({'geneName':list(gene_name_vals)})
+        return jsonify({"subList":rna_df})
     except Exception as e:
         return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
-    
-    #get_heatmap_data()
-    
+      
     app.run(debug=True,host="0.0.0.0",port=5000)
